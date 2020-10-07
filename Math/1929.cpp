f28ffce60d99715f6fbd7a003af3f0780e3110ac
jiwon <wldnjs5818@naver.com>
2020-10-08 00:24:31 +0900

    Create 1929.cpp

---
+++ Math/1929.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int arr[1000001] = {0, };
+
+    for(int i=2;i<=b;i++){
+        if(arr[i] == 1) continue;
+        else{
+            for(int j=i+i;j<=b;j+=i){
+                arr[j] = 1;
+            }
+        }
+    }
+
+    int answer = 0;
+
+    arr[1] = 1;
+    
+    for(int i=a;i<=b;i++){
+        if(arr[i] == 0){
+            printf("%d\n",i);
+        }
+    }
+
+
+    return 0;
+}
