6b24fde9141e58cb1956fc4cd372d90156ae5ccc
jiwon <wldnjs5818@naver.com>
2020-10-08 22:11:11 +0900

    Modified 2309.cpp

--- Undefined/2309.cpp
+++ Undefined/2309.cpp
@@ -7,15 +7,14 @@ using namespace std;
 int main(){
     int arr[9] = {0, };
     int sum = 0;
-    int i, j;
     vector<int> v;
     for(int i=0;i<9;i++){
         scanf("%d", &arr[i]);
         sum += arr[i];
     }
 
-    for(i=0;i<8;i++){
-        for(j=i+1;j<9;j++){
+    for(int i=0;i<8;i++){
+        for(int j=i+1;j<9;j++){
             if(sum - arr[i] - arr[j] == 100){
                 for(int l=0;l<9;l++){
                     // printf("%d %d##",i,j);
@@ -34,12 +33,6 @@ int main(){
             }
         }
     }
-
-    
-
-   
-    
-    
 
     return 0;
 }
